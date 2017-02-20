a9ae566729338f90531eac8a7a31e733685e272b
Vidushi <singh.vidushi@rocketmail.com>
2017-02-20 12:54:24 -0800

    This is the code for the Gear Lifter I have

--- src/Team4450/Robot10/GearLifter.java
+++ src/Team4450/Robot10/GearLifter.java
@@ -1,5 +1,79 @@
 package Team4450.Robot10;
 
-public class GearLifter {
+import edu.wpi.first.wpilibj.Talon;
+import Team4450.Lib.*;
+import edu.wpi.first.wpilibj.smartdashboard.*;
 
+public class GearLifter {
+	private final Robot				robot;
+	private final Teleop			teleop;
+	
+	private final Talon			    gearPickupMotor = new Talon(1);
+	private final ValveDA			gearLifter = new ValveDA(6);
+	private final ValveDA			gearDeploy = new ValveDA(1,0); //in PCM 1 ports
+	
+	boolean gearDown = false;
+	boolean gearIn = false;
+	
+	public GearLifter(Robot robot, Teleop teleop){
+		this.robot = robot;
+		this.teleop = teleop;
+		
+	}
+	public void Dispose(){
+		Util.consoleLog();
+		if(gearPickupMotor != null) gearPickupMotor.free();
+		if(gearLifter != null ) gearLifter.dispose();
+		if(gearDeploy != null) gearDeploy.dispose();
+		
+	}
+	public void startGearIn(){
+		Util.consoleLog();
+		
+		gearPickupMotor.set(.5);
+	}
+	public void startGearOut(){
+		Util.consoleLog();
+		
+		gearPickupMotor.set(-.2);
+		
+	}
+	
+	public void GearIn(){
+		if(gearIn = false){
+			Util.consoleLog();
+			gearDeploy.SetA();
+			
+			gearIn = true;
+		}
+	}
+	
+    public void GearOut(){
+		if(gearIn = true){
+			Util.consoleLog();
+			gearDeploy.SetB();
+			
+			gearIn = false;
+		}
+	}
+	public void GearDown(){
+		if(gearDown = false){
+			Util.consoleLog();
+			gearLifter.SetB();
+			
+			gearDown = true;
+		}
+	
+		}
+	
+	public void GearUp(){
+		if(gearDown = true){
+			Util.consoleLog();
+			gearLifter.SetA();
+			
+			gearDown = false;
+		}
+		
+	}
+	
 }
